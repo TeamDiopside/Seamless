0fcff26f8fa97cb36e5d8f7915e337b0909955cb
LarsMans <l.s.vanemmerik@gmail.com>
2023-05-03 16:13:09 +0200

    ehhhhh

---
+++ forge/src/main/java/nl/curryducker/seamless/forge/mixin/BedrollBlockMixin.java
@@ -0,0 +1,36 @@
+package nl.curryducker.seamless.forge.mixin;
+
+import com.teamabnormals.upgrade_aquatic.common.block.BedrollBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import nl.curryducker.seamless.Voxelshapes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BedrollBlock.class)
+public class BedrollBlockMixin extends BedBlock {
+    @Shadow @Final private static VoxelShape SHAPE;
+
+    public BedrollBlockMixin(DyeColor arg, Properties arg2) {
+        super(arg, arg2);
+    }
+
+    @Inject(method = "getShape", at = @At("HEAD"), cancellable = true)
+    public void getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext, CallbackInfoReturnable<VoxelShape> cir) {
+        cir.setReturnValue(Voxelshapes.bedroll(blockState.getValue(FACING), blockState.getValue(PART)));
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState arg, BlockGetter arg2, BlockPos arg3, CollisionContext arg4) {
+        return SHAPE;
+    }
+}
