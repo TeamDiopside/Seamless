f10b539890c8da15b3a94e0f3c7d937fe15bab2e
LarsMans <l.s.vanemmerik@gmail.com>
2023-05-19 23:45:14 +0200

    things

--- common/src/main/java/nl/curryducker/seamless/Seamless.java
+++ common/src/main/java/nl/curryducker/seamless/Seamless.java
@@ -1,7 +1,11 @@
 package nl.curryducker.seamless;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Seamless {
     public static final String MOD_ID = "seamless";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     
     public static void init() {
     }
