25c0170f7e3889305d07747feec003bac3c68f0e
LarsMans <l.s.vanemmerik@gmail.com>
2023-08-26 14:09:49 +0200

    good commit :)

--- common/src/main/java/nl/teamdiopside/seamless/OutlineReloadListener.java
+++ common/src/main/java/nl/teamdiopside/seamless/OutlineReloadListener.java
@@ -4,17 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
-import net.minecraft.util.RandomSource;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.block.Block;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class OutlineReloadListener extends SimpleJsonResourceReloadListener {
 
@@ -22,8 +20,7 @@ public class OutlineReloadListener extends SimpleJsonResourceReloadListener {
         super(new Gson(), "outline_rules");
     }
 
-    public record OutlineRule(List<Block> blocks, HashMap<String, List<String>> blockstates, List<Direction> directions, List<Block> connectingBlocks, HashMap<String, List<String>> connectingBlockstates) {
-    }
+    public record OutlineRule(Set<Block> blocks, HashMap<String, Set<String>> blockstates, Set<Direction> directions, Set<Block> connectingBlocks, HashMap<String, Set<String>> connectingBlockstates) {}
 
     public static final List<OutlineRule> RULES = new ArrayList<>();
 
@@ -33,39 +30,49 @@ public class OutlineReloadListener extends SimpleJsonResourceReloadListener {
         List<OutlineRule> temp = new ArrayList<>();
         jsons.forEach((key, json) -> {
             try {
-                List<Block> blocks = new ArrayList<>();
-                json.getAsJsonObject().get("blocks").getAsJsonArray().forEach(jsonElement ->
-                        blocks.add(BuiltInRegistries.BLOCK.get(new ResourceLocation(jsonElement.getAsString())))
-                );
+                Set<Block> blocks = new HashSet<>();
+                json.getAsJsonObject().get("blocks").getAsJsonArray().forEach(jsonElement -> {
+                    if (jsonElement.getAsString().startsWith("#")) {
+                        TagKey<Block> blockTagKey = TagKey.create(Registries.BLOCK, new ResourceLocation(jsonElement.getAsString().replace("#", "")));
+                        BuiltInRegistries.BLOCK.getOrCreateTag(blockTagKey).stream().forEach(blockHolder -> blocks.add(blockHolder.value()));
+                    } else {
+                        blocks.add(BuiltInRegistries.BLOCK.get(new ResourceLocation(jsonElement.getAsString())));
+                    }
+                });
 
-                HashMap<String, List<String>> blockstates = new HashMap<>();
+                HashMap<String, Set<String>> blockstates = new HashMap<>();
                 json.getAsJsonObject().get("blockstates").getAsJsonObject().asMap().forEach((k, v) -> {
-                    List<String> states = new ArrayList<>();
+                    Set<String> states = new HashSet<>();
                     v.getAsJsonArray().forEach(jsonElement -> states.add(jsonElement.getAsString()));
                     blockstates.put(k, states);
                 });
 
-                List<Direction> directions = new ArrayList<>();
+                Set<Direction> directions = new HashSet<>();
                 json.getAsJsonObject().get("directions").getAsJsonArray().forEach(jsonElement ->
                         directions.add(Direction.byName(jsonElement.getAsString()))
                 );
 
-                List<Block> connectingBlocks = new ArrayList<>();
-                json.getAsJsonObject().get("connecting_blocks").getAsJsonArray().forEach(jsonElement ->
-                        connectingBlocks.add(BuiltInRegistries.BLOCK.get(new ResourceLocation(jsonElement.getAsString())))
-                );
+                Set<Block> connectingBlocks = new HashSet<>();
+                json.getAsJsonObject().get("connecting_blocks").getAsJsonArray().forEach(jsonElement -> {
+                    if (jsonElement.getAsString().startsWith("#")) {
+                        TagKey<Block> blockTagKey = TagKey.create(Registries.BLOCK, new ResourceLocation(jsonElement.getAsString().replace("#", "")));
+                        BuiltInRegistries.BLOCK.getOrCreateTag(blockTagKey).stream().forEach(blockHolder ->
+                                connectingBlocks.add(blockHolder.value())
+                        );
+                    } else {
+                        connectingBlocks.add(BuiltInRegistries.BLOCK.get(new ResourceLocation(jsonElement.getAsString())));
+                    }
+                });
 
-                HashMap<String, List<String>> connectingBlockstates = new HashMap<>();
+                HashMap<String, Set<String>> connectingBlockstates = new HashMap<>();
                 json.getAsJsonObject().get("connecting_blockstates").getAsJsonObject().asMap().forEach((k, v) -> {
-                    List<String> states = new ArrayList<>();
+                    Set<String> states = new HashSet<>();
                     v.getAsJsonArray().forEach(jsonElement -> states.add(jsonElement.getAsString()));
                     connectingBlockstates.put(k, states);
                 });
 
                 temp.add(new OutlineRule(blocks, blockstates, directions, connectingBlocks, connectingBlockstates));
-
-                Seamless.LOGGER.info("Found outline rule " + key.getPath());
-
+                Seamless.LOGGER.info("Found outline rule " + key.getPath() + ".json");
             } catch (Exception e) {
                 Seamless.LOGGER.error("Failed to parse JSON object for outline rule " + key + ", Cause: " + e);
             }
@@ -74,5 +81,34 @@ public class OutlineReloadListener extends SimpleJsonResourceReloadListener {
         RULES.addAll(temp);
     }
 
-    public static final RandomSource rs = RandomSource.create();
+    public static Set<Block> getBlocks(JsonElement json, String key) {
+        Set<Block> blocks = new HashSet<>();
+        json.getAsJsonObject().get(key).getAsJsonArray().forEach(jsonElement -> {
+            if (jsonElement.getAsString().startsWith("#")) {
+                TagKey<Block> blockTagKey = TagKey.create(Registries.BLOCK, new ResourceLocation(jsonElement.getAsString().replace("#", "")));
+                BuiltInRegistries.BLOCK.getOrCreateTag(blockTagKey).stream().forEach(blockHolder -> blocks.add(blockHolder.value()));
+            } else {
+                blocks.add(BuiltInRegistries.BLOCK.get(new ResourceLocation(jsonElement.getAsString())));
+            }
+        });
+        return blocks;
+    }
+
+    public static HashMap<String, Set<String>> getBlockStates(JsonElement json, String key) {
+        HashMap<String, Set<String>> blockstates = new HashMap<>();
+        json.getAsJsonObject().get(key).getAsJsonObject().asMap().forEach((k, v) -> {
+            Set<String> states = new HashSet<>();
+            v.getAsJsonArray().forEach(jsonElement -> states.add(jsonElement.getAsString()));
+            blockstates.put(k, states);
+        });
+        return blockstates;
+    }
+
+    public static Set<Direction> getDirections(JsonElement json, String key) {
+        Set<Direction> directions = new HashSet<>();
+        json.getAsJsonObject().get(key).getAsJsonArray().forEach(jsonElement ->
+                directions.add(Direction.byName(jsonElement.getAsString()))
+        );
+        return directions;
+    }
 }
--- common/src/main/java/nl/teamdiopside/seamless/mixin/LevelRendererMixin.java
+++ common/src/main/java/nl/teamdiopside/seamless/mixin/LevelRendererMixin.java
@@ -10,6 +10,8 @@ import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -18,32 +20,33 @@ import nl.teamdiopside.seamless.Recursion;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static nl.teamdiopside.seamless.OutlineReloadListener.RULES;
 
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin implements ResourceManagerReloadListener, AutoCloseable {
+
     @Shadow private @Nullable ClientLevel level;
 
     @Inject(method = "renderHitOutline", at = @At("HEAD"), cancellable = true)
     private void renderHitOutline(PoseStack poseStack, VertexConsumer vertexConsumer, Entity entity, double i, double b, double c, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-
-        Recursion recursion = recursiveShape(level, blockState, blockPos, new HashSet<>(), blockPos, entity);
-        VoxelShape shape = recursion.voxelShape();
+        Recursion recursion = findAndAddShapes(level, blockState, blockPos, new HashSet<>(), blockPos, entity);
+        VoxelShape shape = recursion.voxelShape().optimize();
 
         LevelRenderer.renderShape(poseStack, vertexConsumer, shape, (double)blockPos.getX() - i, (double)blockPos.getY() - b, (double)blockPos.getZ() - c, 0.0f, 0.0f, 0.0f, 0.4f);
         ci.cancel();
     }
 
-    private static Recursion recursiveShape(Level level, BlockState state, BlockPos pos, Set<BlockPos> connectedPositions, BlockPos originalPos, Entity entity) {
+    @Unique
+    private static Recursion findAndAddShapes(Level level, BlockState state, BlockPos pos, Set<BlockPos> connectedPositions, BlockPos originalPos, Entity entity) {
         connectedPositions.add(pos);
         BlockPos relativePos = pos.subtract(originalPos);
         VoxelShape shape = state.getShape(level, pos, CollisionContext.of(entity)).move(relativePos.getX(), relativePos.getY(), relativePos.getZ());
@@ -52,6 +55,22 @@ public abstract class LevelRendererMixin implements ResourceManagerReloadListene
             if (!outlineRule.blocks().contains(state.getBlock())) {
                 continue;
             }
+
+            boolean blockstatesMatch = true;
+            for (HashMap.Entry<String, Set<String>> entry : outlineRule.blockstates().entrySet()) {
+                Property<?> property = state.getBlock().getStateDefinition().getProperty(entry.getKey());
+                assert property != null : "Blockstate property " + entry.getKey() + " does not exist for " + state.getBlock().getName();
+                boolean propertyEquals = entry.getKey().equals(property.getName());
+                boolean valueEquals = entry.getValue().contains(state.getValue(property).toString());
+                if (!(propertyEquals && valueEquals)) {
+                    blockstatesMatch = false;
+                    break;
+                }
+            }
+            if (!blockstatesMatch) {
+                continue;
+            }
+
             for (Direction direction : outlineRule.directions()) {
                 BlockPos checkingPos = pos.relative(direction);
                 BlockState checkingState = level.getBlockState(checkingPos);
@@ -60,24 +79,35 @@ public abstract class LevelRendererMixin implements ResourceManagerReloadListene
                     continue;
                 }
 
-                boolean blockstatesMatch = true;
-                for (HashMap.Entry<String, List<String>> entry : outlineRule.connectingBlockstates().entrySet()) {
-                    if (!entry.getValue().contains(checkingState.getValue(checkingState.getBlock().getStateDefinition().getProperty(entry.getKey())).toString())) {
-                        blockstatesMatch = false;
+                boolean connectingBlockstatesMatch = true;
+                for (HashMap.Entry<String, Set<String>> entry : outlineRule.connectingBlockstates().entrySet()) {
+                    Property<?> property = checkingState.getBlock().getStateDefinition().getProperty(entry.getKey());
+                    assert property != null : "Blockstate property " + entry.getKey() + " does not exist for " + checkingState.getBlock().getName();
+                    boolean propertyEquals = entry.getKey().equals(property.getName());
+                    boolean valueEquals = entry.getValue().contains(checkingState.getValue(property).toString());
+                    if (!(propertyEquals && valueEquals)) {
+                        connectingBlockstatesMatch = false;
                         break;
                     }
                 }
-                if (!blockstatesMatch) {
+                if (!connectingBlockstatesMatch) {
                     continue;
                 }
 
-                Recursion recursion = recursiveShape(level, checkingState, checkingPos, connectedPositions, originalPos, entity);
-                shape = Shapes.or(shape, recursion.voxelShape());
+                Recursion recursion = findAndAddShapes(level, checkingState, checkingPos, connectedPositions, originalPos, entity);
+                shape = Shapes.joinUnoptimized(shape, recursion.voxelShape(), BooleanOp.OR);
                 connectedPositions = recursion.connectedPositions();
             }
         }
         return new Recursion(shape, connectedPositions);
     }
+
+    @Unique
+    private static boolean propertyDoesntMatch(BlockState blockState, String propertyName, Set<String> values) {
+        Property<?> property = blockState.getBlock().getStateDefinition().getProperty(propertyName);
+        assert property != null : "Blockstate property " + propertyName + " does not exist for " + blockState.getBlock().getName();
+        boolean propertyEquals = propertyName.equals(property.getName());
+        boolean valueEquals = values.contains(blockState.getValue(property).toString());
+        return !(propertyEquals && valueEquals);
+    }
 }
-
-
