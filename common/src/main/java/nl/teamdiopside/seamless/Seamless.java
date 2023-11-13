bb40cda2873b0b2570c4f61caa5c84f0c8d0af3c
LarsMans <l.s.vanemmerik@gmail.com>
2023-11-13 22:38:56 +0100

    fix 1.19.2

--- common/src/main/java/nl/teamdiopside/seamless/Seamless.java
+++ common/src/main/java/nl/teamdiopside/seamless/Seamless.java
@@ -2,7 +2,6 @@ package nl.teamdiopside.seamless;
 
 import dev.architectury.platform.Platform;
 import net.minecraft.client.OptionInstance;
-import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +16,7 @@ public class Seamless {
     public static List<String> modIds = new ArrayList<>();
 
     public static boolean fastEnabled = false;
-    public static OptionInstance<Boolean> fastOption = OptionInstance.createBoolean("options.fast_seamless", bool -> Tooltip.create(Component.translatable("options.fast_seamless.tooltip")), OptionInstance.BOOLEAN_TO_STRING, false, aBoolean -> fastEnabled = aBoolean);
+    public static OptionInstance<Boolean> fastOption = OptionInstance.createBoolean("options.fast_seamless", OptionInstance.cachedConstantTooltip(Component.translatable("options.fast_seamless.tooltip")), false, aBoolean -> fastEnabled = aBoolean);
 
     public static void init() {
         modIds = Platform.getModIds().stream().toList();
