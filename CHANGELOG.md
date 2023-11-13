26a3cfdc9de11c1c14d7af971d615ffd697de689
LarsMans <l.s.vanemmerik@gmail.com>
2023-11-13 22:02:07 +0100

    improve performance a whole lot + client badge

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Seamless Changelog
 
+## 2.2.0
+- Improved performance
+- Added a new option in the video settings, called "Fast Seamless"
+  - Enabling this option simplifies the way seamless checks for blocks, improving performance, but it has some side effects
+- The mod now has the client badge in ModMenu
+
 ## 2.1.1
 - Fixed log spam when Sophisticated Storage is installed
 - The built-in resource pack should now work correctly on 1.20.2
--- common/src/main/java/nl/teamdiopside/seamless/Seamless.java
+++ common/src/main/java/nl/teamdiopside/seamless/Seamless.java
@@ -1,13 +1,25 @@
 package nl.teamdiopside.seamless;
 
+import dev.architectury.platform.Platform;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Seamless {
     public static final String MOD_ID = "seamless";
     public static final String RESOURCE_PACK = "default_seamless";
     public static final Logger LOGGER = LoggerFactory.getLogger("Seamless");
+    public static List<String> modIds = new ArrayList<>();
+
+    public static boolean fastEnabled = false;
+    public static OptionInstance<Boolean> fastOption = OptionInstance.createBoolean("options.fast_seamless", bool -> Tooltip.create(Component.translatable("options.fast_seamless.tooltip")), OptionInstance.BOOLEAN_TO_STRING, false, aBoolean -> fastEnabled = aBoolean);
 
     public static void init() {
+        modIds = Platform.getModIds().stream().toList();
     }
 }
