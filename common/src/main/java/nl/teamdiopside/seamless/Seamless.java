89adf4e6531691d435a8a43a90f96550f72dbeb5
LarsMans <l.s.vanemmerik@gmail.com>
2023-11-24 23:51:57 +0100

    build.gradle

--- common/src/main/java/nl/teamdiopside/seamless/Seamless.java
+++ common/src/main/java/nl/teamdiopside/seamless/Seamless.java
@@ -2,6 +2,8 @@ package nl.teamdiopside.seamless;
 
 import dev.architectury.platform.Platform;
 import net.minecraft.client.CycleOption;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,8 +19,8 @@ public class Seamless {
     public static List<String> modIds = new ArrayList<>();
     public static Set<String> errors = new HashSet<>();
 
-    public static final Component FAST_SEAMLESS_TEXT = Component.translatable("options.fast_seamless");
-    public static final Component FAST_SEAMLESS_TOOLTIP = Component.translatable("options.fast_seamless.tooltip");
+    public static final Component FAST_SEAMLESS_TEXT = new TranslatableComponent("options.fast_seamless");
+    public static final Component FAST_SEAMLESS_TOOLTIP = new TranslatableComponent("options.fast_seamless.tooltip");
 
     public static boolean fastEnabled = false;
     public static CycleOption<Boolean> fastOption = CycleOption.createOnOff("options.fast_seamless", (options) -> fastEnabled, (options, option, object) -> fastEnabled = object);
