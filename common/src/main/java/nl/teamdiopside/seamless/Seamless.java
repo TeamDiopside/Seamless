6336e406b7ed05cefc7064659b35eff1e10d1562
LarsMans <l.s.vanemmerik@gmail.com>
2023-11-24 23:05:22 +0100

    sodium and errors

--- common/src/main/java/nl/teamdiopside/seamless/Seamless.java
+++ common/src/main/java/nl/teamdiopside/seamless/Seamless.java
@@ -8,16 +8,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Seamless {
     public static final String MOD_ID = "seamless";
     public static final String RESOURCE_PACK = "default_seamless";
     public static final Logger LOGGER = LoggerFactory.getLogger("Seamless");
     public static List<String> modIds = new ArrayList<>();
+    public static Set<String> errors = new HashSet<>();
+
+    public static final Component FAST_SEAMLESS_TEXT = Component.translatable("options.fast_seamless");
+    public static final Component FAST_SEAMLESS_TOOLTIP = Component.translatable("options.fast_seamless.tooltip");
 
     public static boolean fastEnabled = false;
-    public static OptionInstance<Boolean> fastOption = OptionInstance.createBoolean("options.fast_seamless", bool -> Tooltip.create(Component.translatable("options.fast_seamless.tooltip")), OptionInstance.BOOLEAN_TO_STRING, false, aBoolean -> fastEnabled = aBoolean);
+    public static OptionInstance<Boolean> fastOption = OptionInstance.createBoolean("options.fast_seamless", bool -> Tooltip.create(FAST_SEAMLESS_TOOLTIP), OptionInstance.BOOLEAN_TO_STRING, false, aBoolean -> fastEnabled = aBoolean);
 
     public static void init() {
         modIds = Platform.getModIds().stream().toList();
